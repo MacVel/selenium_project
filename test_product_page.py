b1e2b4d65a311a12aa79fefd3cfed4e5727b999a
Max Kortusov <maxkortusov@gmail.com>
2020-08-08 13:40:27 +0600

    change link in test_guest_cant_see_product_in_basket

--- test_product_page.py
+++ test_product_page.py
@@ -65,7 +65,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser,link):
 
 @pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
     page = BasketPage(browser,link)
     page.open()
     page.go_to_basket()
